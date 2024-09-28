56fa0fd0e479e3e64fd448e488025ad593d0b22e
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-09-27 22:46:55 -0300

    criado os enumeradores

---
+++ Enumeradores/Menu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoEscola.Enumeradores
+{
+    public enum Menu
+    {
+        Cadastrar = 1,
+        Listar = 2,
+        Editar = 3,
+        Apagar = 4,
+    }
+}
---
+++ Enumeradores/SubMenu.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoEscola.Enumeradores
+{
+    public enum SubMenu
+    {
+        Pessoa = 1,
+        Aluno = 2,
+        Instrutor = 3,
+    }
+}
