c8839c8707a27aeb977feeb2a1d57b8d2cc02486
Thykkanno <lucianotadeubs@yahoolcom.br>
2024-09-27 23:39:25 -0300

    criada a class pessoa

--- Entidades/Pessoa.cs
+++ Entidades/Pessoa.cs
@@ -8,6 +8,40 @@ namespace AutoEscola.Entidades
 {
     internal class Pessoa
     {
+        public static int id;
 
+        private int _id;
+
+        public int Id
+        {
+            get { return _id; }
+        }
+
+
+        public string Nome { get; set; }
+        public DateTime Nascimento { get; set; }
+        public string Cpf { get; set; }
+        public bool Ativo { get; set; }
+
+        public Pessoa()
+        {
+            id++;
+            _id = id;
+        }
+
+        public Pessoa(string nome, DateTime nascimento, string cpf) : this()
+        {
+            Nome = nome;
+            Nascimento = nascimento;
+            Cpf = cpf;
+            Ativo = true;
+        }
+
+        public virtual string ImprimeDadosPessoais()
+        {
+            return $"Nome: {Nome}\n" +
+                $"Idade : {DateTime.Now.Year - Nascimento.Year}\n" +
+                $"Cpf: {Cpf}";
+        }
     }
 }
